73006d274bed4880f34124ace5859ff0e596b841
Brandon Keepers <brandon@opensoul.org>
2014-12-30 01:36:30 -0500

    Move transpose logic into chord model

--- lib/chordpro/chord.rb
+++ lib/chordpro/chord.rb
@@ -1,5 +1,12 @@
 module Chordpro
   class Chord < Struct.new(:name)
+    Notes = %w(C Cb C# D Db D# E Eb E# F Fb F# G Gb G# A Ab A# B Bb B#)
+
+    Transpositions = %w(Cb C C# Bb Cb C C C# D Db D D# C Db D D D# E Eb E F D Eb
+      E E E# F# E F F# Eb Fb F F F# G Gb G G# F Gb G G G# A Ab A A# G Ab A A A#
+      B Bb B C A Bb B B B# C#
+    )
+
     Substitutions = {
       'b' => '♭',
       '#' => '♯',
@@ -22,6 +29,18 @@ module Chordpro
 
     def accept(visitor)
       visitor.respond_to?(:chord) ? visitor.chord(self) : self
+    end
+
+    def transpose(interval)
+      new_name = name.gsub(/([A-G][#b]?)/) do |match|
+        interval.abs.times do
+          index = Notes.index(match)
+          match = Transpositions[3 * index - 2 + 3 + (interval <=> 0)]
+        end
+        match
+      end
+
+      Chord.new(new_name)
     end
   end
 end
--- lib/chordpro/transposer.rb
+++ lib/chordpro/transposer.rb
@@ -1,12 +1,5 @@
 module Chordpro
   class Transposer
-    NOTES = %w(C Cb C# D Db D# E Eb E# F Fb F# G Gb G# A Ab A# B Bb B#)
-
-    TRANSPOSITIONS = %w(Cb C C# Bb Cb C C C# D Db D D# C Db D D D# E Eb E F D Eb
-      E E E# F# E F F# Eb Fb F F F# G Gb G G# F Gb G G G# A Ab A A# G Ab A A A#
-      B Bb B C A Bb B B B# C#
-    )
-
     def initialize(song, interval)
       @song = song
       @interval = interval
@@ -17,17 +10,7 @@ module Chordpro
     end
 
     def chord(chord)
-      Chord.new(transpose(chord))
-    end
-
-    def transpose(chord)
-      chord.name.gsub(/([A-G][#b]?)/) do |match|
-        @interval.abs.times do
-          index = NOTES.index(match)
-          match = TRANSPOSITIONS[3 * index - 2 + 3 + (@interval <=> 0)]
-        end
-        match
-      end
+      chord.transpose(@interval)
     end
   end
 end
