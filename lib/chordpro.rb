4a832df09299e812d99affba24113ed6d8b69d26
Brandon Keepers <brandon@opensoul.org>
2022-02-06 09:45:38 -0500

    Add Chordpro.parse

--- lib/chordpro.rb
+++ lib/chordpro.rb
@@ -4,13 +4,20 @@ require "chordpro/html"
 require "chordpro/line"
 require "chordpro/linebreak"
 require "chordpro/lyric"
+require "chordpro/metadata"
 require "chordpro/parser"
 require "chordpro/song"
 require "chordpro/transform"
 require "chordpro/version"
 
+require "chordpro/setup"
+
 module Chordpro
+  def self.parse(string)
+    Transform.new.apply(Parser.new.parse(string))
+  end
+
   def self.html(string)
-    HTML.new(Transform.new.apply(Parser.new.parse(string))).to_s
+    HTML.new(parse(string)).to_s
   end
 end
